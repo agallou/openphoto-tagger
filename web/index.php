787b976147b4ade112ebb9efecb6cd2f32c5f2bf
Adrien Gallou <adriengallou@gmail.com>
2013-12-08 02:26:33 +0100

    fix code conventions

--- web/index.php
+++ web/index.php
@@ -5,13 +5,18 @@ use Symfony\Component\Yaml\Parser;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-
 $app = new Silex\Application();
 
 
 $yaml      = new Parser();
 $config    = $yaml->parse(file_get_contents('../config/config.yml'));
-$openphoto = new OpenPhotoOAuth($config['host'], $config['consumerKey'], $config['consumerSecret'], $config['token'], $config['tokenSecret']);
+$openphoto = new OpenPhotoOAuth(
+    $config['host'],
+    $config['consumerKey'],
+    $config['consumerSecret'],
+    $config['token'],
+    $config['tokenSecret']
+);
 $app['openphoto'] = $openphoto;
 
 $app->register(new Silex\Provider\UrlGeneratorServiceProvider());
@@ -31,56 +36,54 @@ $app->get('/logout', function () use ($app) {
 })->bind('logout');
 
 $app->get('/login', function () use ($app) {
-  if ($app['session']->has('user')) {
-    return $app->redirect($app['url_generator']->generate('homepage'));
-  }
+    if ($app['session']->has('user')) {
+        return $app->redirect($app['url_generator']->generate('homepage'));
+    }
 
-  return $app['twig']->render('login.twig', array(
-  ));
+    return $app['twig']->render('login.twig');
 
 })->bind('login');
 
 $app->post('/api/login', function () use ($app) {
 
-  $yaml      = new Parser();
-  $config    = $yaml->parse(file_get_contents(__DIR__ . '/../config/browserid.yml'));
-  $security  = $yaml->parse(file_get_contents(__DIR__ . '/../config/security.yml'));
+    $yaml      = new Parser();
+    $config    = $yaml->parse(file_get_contents(__DIR__ . '/../config/browserid.yml'));
+    $security  = $yaml->parse(file_get_contents(__DIR__ . '/../config/security.yml'));
 
 
 
-  $url  = 'https://browserid.org/verify';
-  $data = http_build_query(array(
-    'assertion' => $_POST['assertion'],
-    'audience' => urlencode($config['audience'])
+    $url  = 'https://browserid.org/verify';
+    $data = http_build_query(array(
+      'assertion' => $_POST['assertion'],
+      'audience' => urlencode($config['audience'])
 
-  ));
+    ));
 
-  $params = array(
-    'http' => array(
-      'method'    => 'POST',
-        'content' => $data,
-        'header'  =>
-          "Content-type: application/x-www-form-urlencoded\r\n"
-          . "Content-Length: " . strlen($data) . "\r\n"
-    )
-  );
+    $params = array(
+      'http' => array(
+        'method'    => 'POST',
+          'content' => $data,
+          'header'  =>
+            "Content-type: application/x-www-form-urlencoded\r\n"
+            . "Content-Length: " . strlen($data) . "\r\n"
+      )
+    );
 
-  $ctx = stream_context_create($params);
-  $fp  = fopen($url, 'rb', false, $ctx);
+    $ctx = stream_context_create($params);
+    $fp  = fopen($url, 'rb', false, $ctx);
 
-  if ($fp) {
-    $result = stream_get_contents($fp);
-  }  else {
-    $result = FALSE;
-  }
-  $json = json_decode($result);
-  if ($json->status == 'okay' && in_array($json->email, $security['users']))
-  {
-    $app['session']->start();
-    $app['session']->set('user', array('username' => $json->email));
-  }
+    if ($fp) {
+        $result = stream_get_contents($fp);
+    } else {
+        $result = false;
+    }
+    $json = json_decode($result);
+    if ($json->status == 'okay' && in_array($json->email, $security['users'])) {
+        $app['session']->start();
+        $app['session']->set('user', array('username' => $json->email));
+    }
 
-  return $result;
+    return $result;
 });
 
 
@@ -103,80 +106,73 @@ $app['debug'] = true;
 
 $app->get('/photo/{id}/display', function ($id) use ($app, $config) {
 
-  $photo = $app['openphoto']->get(sprintf('/photo/%s/view.json', $id), array('returnSizes' => '700x700'));
-  $photo = json_decode($photo);
-  $photo = $photo->result;
+    $photo = $app['openphoto']->get(sprintf('/photo/%s/view.json', $id), array('returnSizes' => '700x700'));
+    $photo = json_decode($photo);
+    $photo = $photo->result;
 
-  $path = $photo->path700x700;
+    $path = $photo->path700x700;
 
-  $tokenTags = array();
-  foreach ($photo->tags as $tag)
-  {
-    $tokenTags[] = array('id' => $tag, 'name' => $tag);
-  }
-
-
-  $tags = $app['openphoto']->get('/tags/list.json');
-  $tags = json_decode($tags);
-  $displayedTags = array();
-  $nbTag = 0;
-  foreach ($tags->result as $tag)
-  {
-    if (isset($config['tags']) && $tag->id == $config['tags'])
-    {
-      $nbTag = $tag->count;
+    $tokenTags = array();
+    foreach ($photo->tags as $tag) {
+        $tokenTags[] = array('id' => $tag, 'name' => $tag);
     }
-  }
 
-  return $app['twig']->render('index.twig', array(
-    'path'  => $path,
-    'tags'  => $tokenTags,
-    'id'    => $photo->id,
-    'next'  => $app['url_generator']->generate('homepage'),
-    'nb'    => $nbTag,
-    'date'  => $photo->dateTaken,
-    'title' => $photo->title,
-  ));
+
+    $tags = $app['openphoto']->get('/tags/list.json');
+    $tags = json_decode($tags);
+    $displayedTags = array();
+    $nbTag = 0;
+    foreach ($tags->result as $tag) {
+        if (isset($config['tags']) && $tag->id == $config['tags']) {
+            $nbTag = $tag->count;
+        }
+    }
+
+    return $app['twig']->render('index.twig', array(
+      'path'  => $path,
+      'tags'  => $tokenTags,
+      'id'    => $photo->id,
+      'next'  => $app['url_generator']->generate('homepage'),
+      'nb'    => $nbTag,
+      'date'  => $photo->dateTaken,
+      'title' => $photo->title,
+    ));
 })->before($mustBeLogged)->bind('photo_display');
 
 
 $app->post('/photo/{id}/update', function (Request $request) use ($app, $openphoto) {
-  $id    = $request->get('id');
-  $tags  = $request->get('tags');
-  $photo = $app['openphoto']->post(sprintf('/photo/%s/update.json', $id), array('tags' => $tags));
+    $id    = $request->get('id');
+    $tags  = $request->get('tags');
+    $photo = $app['openphoto']->post(sprintf('/photo/%s/update.json', $id), array('tags' => $tags));
 })->before($mustBeLogged);
 
 
 $app->get('/', function () use ($app, $config) {
-  $options = array('pageSize' => 1);
-  if (isset($config['tags']))
-  {
-    $options['tags'] = $config['tags'];
-  }
+    $options = array('pageSize' => 1);
+    if (isset($config['tags'])) {
+        $options['tags'] = $config['tags'];
+    }
 
+    $photos = $app['openphoto']->get('/photos/list.json', $options);
+    $photos = json_decode($photos);
+    $photos = $photos->result;
 
-  $photos = $app['openphoto']->get('/photos/list.json', $options);
-  $photos = json_decode($photos);
-  $photos = $photos->result;
-
-  $photo = $photos[0];
-  return sprintf('<meta http-equiv="refresh" content="0; url=%s" />', $app['url_generator']->generate('photo_display', array('id' => $photo->id)));
+    $photo = $photos[0];
+    $url = $app['url_generator']->generate('photo_display', array('id' => $photo->id));
+    return sprintf('<meta http-equiv="refresh" content="0; url=%s" />', $url);
 })->before($mustBeLogged)->bind('homepage');
 
 $app->get('/tags', function (Request $request) use ($app, $config) {
-  $tags = $app['openphoto']->get('/tags/list.json');
-  $tags = json_decode($tags);
-  $displayedTags = array();
-  $query = $request->get('q');
-  foreach ($tags->result as $tag)
-  {
-    if (null === $query || false !== strpos(strtolower($tag->id), strtolower($query)))
-    {
-      $displayedTags[] = array('id' => $tag->id, 'name' => $tag->id);
+    $tags = $app['openphoto']->get('/tags/list.json');
+    $tags = json_decode($tags);
+    $displayedTags = array();
+    $query = $request->get('q');
+    foreach ($tags->result as $tag) {
+        if (null === $query || false !== strpos(strtolower($tag->id), strtolower($query))) {
+            $displayedTags[] = array('id' => $tag->id, 'name' => $tag->id);
+        }
     }
-  }
-  return json_encode($displayedTags);
+    return json_encode($displayedTags);
 })->before($mustBeLogged);
 
 $app->run();
-
