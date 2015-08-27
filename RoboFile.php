0bb7a65f46e4fbc5045af8806e1bdf16258856ed
Adrien Gallou <adriengallou@gmail.com>
2015-08-27 23:23:07 +0200

    conventions

--- RoboFile.php
+++ RoboFile.php
@@ -5,12 +5,15 @@ class RoboFile extends \Robo\Tasks
     public function watch()
     {
         $this->build();
-        $this->taskWatch()
+        $this
+            ->taskWatch()
             ->monitor('Ressources/assets/css/', function () {
                 $this->_cleanBase();
                 $this->_cleanCss();
                 $this->_buildCss();
-            })->run();
+            })
+            ->run()
+        ;
     }
 
     public function build()
@@ -23,22 +26,23 @@ class RoboFile extends \Robo\Tasks
     {
         $this->say("Starting CSS rebuild");
 
-        $this->taskScss([
-            'Ressources/assets/sass/main.scss' => 'cache/sass/main.css'
-        ])->run();
+        $this->taskScss(['Ressources/assets/sass/main.scss' => 'cache/sass/main.css'])->run();
 
-        $this->taskConcat([
-            'Ressources/assets/css/bootstrap.min.css',
-            'Ressources/assets/css/token-input.css',
-            'cache/sass/main.css'
-        ])
-        ->to('cache/main.css')
-        ->run();
+        $this
+            ->taskConcat([
+                'Ressources/assets/css/bootstrap.min.css',
+                'Ressources/assets/css/token-input.css',
+                'cache/sass/main.css'
+            ])
+            ->to('cache/main.css')
+            ->run()
+        ;
 
         $this
             ->taskMinify('cache/main.css')
             ->to('cache/main.css')
-            ->run();
+            ->run()
+        ;
 
         $this->_rename('cache/main.css', 'web/css/main.' . substr(md5_file('cache/main.css'), 0, 5) . '.css');
         $this->say("CSS rebuilt successfully!");
